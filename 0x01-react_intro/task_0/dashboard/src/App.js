72fb676cfbaed2669fe2d3b873b046fef8fdd708
Yasmineltair <yasmineltair293@gmail.com>
2024-06-30 21:52:04 +0300

    00

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
-        <img src={logo} alt="logo" />
+        <img src={ logo } className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
       </div>
       <div className="App-body">
