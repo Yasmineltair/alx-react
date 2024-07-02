7b4e9c3796cb86cc802317872f92ee069685fc0a
Yasmineltair <yasmineltair293@gmail.com>
2024-07-02 17:08:12 +0300

    05

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.jpg';
 import './App.css';
 import { getFullYear, getFooterCopy } from './utils';
+import Notifications from './Notifications';
+
 
 function App() {
   return (
     <div className="App">
+      <Notifications/>
       <div className="App-header">
+        
         <img src={ logo } className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
       </div>
--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -3,23 +3,25 @@ import './Notifications.css';
 import close from './close-icon.png'
 import  { getLatestNotification } from './utils'
 
-export default function notifications() {
+function Notifications() {
     return (
         <div className="Notifications">
             <button 
-                style={{ position: "absolute", right: "2px", top: "2px", cursor: "pointer", fontweight: "bold", border: "none" }}
+                style={{ color: "#3a3a3a", background: "none", position: "absolute", right: "2px", top: "2px", cursor: "pointer", fontweight: "bold", border: "none" }}
                 aria-label="close"
                 onClick={console.log("Close button has been clicked")}
                 >
-                    <img src={ close } alt="close" width="10px"/>
-                </button>
+                <img src={ close } alt="close" width="10px"/>
+            </button>
             <p>Here is the list of notifications</p>
             <ul>
                 <li data="default">New course available</li>
                 <li data="urgent">New resume available</li>
-                <li dangerouslySetInnerHTML={{__html: getLatestNotification() }}></li>
+                <li data="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
             </ul>
             
         </div>
     );
 }
+
+export default Notifications;
