2c245d34016ef5fcdb80685d3158431ad032fa5d
Yasmineltair <yasmineltair293@gmail.com>
2024-07-02 18:21:12 +0300

    task_3

---
+++ 0x01-react_intro/task_3/dashboard/src/utils.test.js
@@ -0,0 +1,15 @@
+import { getFooterCopy, getFullYear, getLatestNotification } from "./utils";
+
+describe('Utils function tests', () => {
+    test('getFullYear returns the current year', () => {
+        const year = new Date().getFullYear();
+        expect(getFullYear()).toBe(year);
+    });
+    test('getFooterCopy returns correct string based on argument', () => {
+        expect(getFooterCopy(true)).toBe('Holberton School');
+        expect(getFooterCopy(false)).toBe('Holberton School main dashboard');
+    });
+    test('getLatestNotification returns correct notification string', () => {
+        expect(getLatestNotification()).toBe('Urgent requirement- complete by EOD')
+    })
+})
