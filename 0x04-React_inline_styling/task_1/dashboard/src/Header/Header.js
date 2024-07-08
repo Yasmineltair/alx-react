8d577e7174b284bd10aaf3eb7c8cb02574514faf
Yasmineltair <yasmineltair293@gmail.com>
2024-07-08 18:27:36 +0300

    01

---
+++ 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import logo from "../assets/holberton-logo.jpg";
+import { Stylesheet, css } from "aphrodite";
+
+function Header() {
+  return (
+    <>
+      <div className={css(styles["App-header"])}>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+    </>
+  );
+}
+
+const styles = Stylesheet.create({
+  "App-header": {
+    fontSize: "1.4rem",
+    color: "#e0354b",
+    display: "flex",
+    alignItems: "center",
+    borderBottom: "3px solid #e0354b",
+  },
+
+  img: {
+    width: "200px",
+    height: "200px",
+  },
+});
+
+export default Header;
