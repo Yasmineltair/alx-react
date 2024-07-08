9ea5a3c8499848f2989fc6bef70493d06e6caa90
Yasmineltair <yasmineltair293@gmail.com>
2024-07-08 19:50:36 +0300

    01

--- 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../assets/holberton-logo.jpg";
-import { Stylesheet, css } from "aphrodite";
+import { StyleSheet, css } from "aphrodite";
 
 function Header() {
   return (
