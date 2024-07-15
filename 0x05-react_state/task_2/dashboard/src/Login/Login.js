f363331275ce12960bda7df69d6f059f893d07d0
Yasmineltair <yasmineltair293@gmail.com>
2024-07-15 17:07:28 +0300

    02

---
+++ 0x05-react_state/task_2/dashboard/src/Login/Login.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import { StyleSheet, css } from "aphrodite";
+import PropTypes from "prop-types";
+
+function Login(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [enableSubmit, setEnableSubmit] = useState(false);
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    setIsLoggedIn(true);
+  };
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  test(() => {
+    if (email !== "" && password !== "") {
+      setEnableSubmit(true);
+    } else {
+      if (enableSubmit !== false) {
+        setEnableSubmit(false);
+      }
+    }
+  }, [email, password]);
+
+  return (
+    <React.Fragment>
+      <div className={css(styles["App-body"])}>
+        <p>Login to access the full dashboard</p>
+        <form onSubmit={handleLoginSubmit}> 
+          <label htmlFor="email">Email:</label>
+          <input className={css(styles.input)} type="email" name="email"></input>
+          <label htmlFor="password">Password:</label>
+          <input className={css(styles.input)} type="password" name="password"></input>
+          <input type="submit" value="ok" display={!enableSubmit}></input>
+        </form>
+      </div>
+    </React.Fragment>
+  );
+}
+
+Login.propTypes = {
+  logIn: PropTypes.func,
+};
+
+const styles = StyleSheet.create({
+  "App-body": {
+    fontSize: "1rem",
+    padding: "2em",
+    height: "45%",
+    "@media (max-width: 900px)": {
+      display: "flex",
+      flexDirection: "column",
+    },
+  },
+
+  input: {
+    margin: "10px",
+  },
+});
+
+export default Login;
